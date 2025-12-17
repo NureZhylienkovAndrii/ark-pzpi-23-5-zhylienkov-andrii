e1d03eddb0a88311996954fe10b0814220278141
Andrii Zhylienkov <169060221+NureZhylienkovAndrii@users.noreply.github.com>
2025-12-17 13:04:27 +0100

    Add files via upload

---
+++ Lab3/ark-pzpi-23-5-zhylienkov-andrii-lab3/Pages/Admin/Cars.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using AutoRent.Data;
+using AutoRent.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoRent.Pages.Admin
+{
+    public class CarsModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+        public CarsModel(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<Car> Cars { get; set; }
+
+        [BindProperty]
+        public int DeleteId { get; set; }
+
+        [BindProperty]
+        public int CarId { get; set; }
+
+        [BindProperty]
+        public int NewStatusId { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Cars = await _db.Cars.Include(c => c.Status).ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync()
+        {
+            var car = await _db.Cars.FindAsync(DeleteId);
+            if (car != null)
+            {
+                _db.Cars.Remove(car);
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostChangeStatusAsync()
+        {
+            var car = await _db.Cars.FindAsync(CarId);
+            if (car != null)
+            {
+                car.StatusId = NewStatusId;
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+    }
+}
