ceaf6be24b509529fac2ad05a976544c38aad5af
Andrii Zhylienkov <169060221+NureZhylienkovAndrii@users.noreply.github.com>
2025-12-20 17:10:18 +0100

    Delete Lab4/Models directory

--- Lab4/Models/CarStatus.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AutoRent.Models
-{
-    public class CarStatus
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = null!;
-        public List<Car> Cars { get; set; } = new();
-    }
-}
